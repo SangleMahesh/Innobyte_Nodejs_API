bc0efc643b2e07db6c24d197784d2e92b745dedf
Mahesh Sangle <maheshsangle431@gmail.com>
2024-03-16 20:42:17 +0530

    sign in handler

---
+++ controllers/signInHandler.js
@@ -0,0 +1,2 @@
+async function handleSignIn(request, response) {}
+module.exports = handleSignIn;
--- controllers/signUpHandler.js
+++ controllers/signUpHandler.js
@@ -1,18 +1,28 @@
 const User = require("../models/user");
+const JWT = require("jsonwebtoken");
 
 async function handleSignup(request, response) {
   const { username, email, password } = request.body;
-  const user = User.create({
-    username,
-    email,
-    password,
-  });
   try {
-    (await user).save();
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+    await user.save();
+
+    const payload = {
+      name: username,
+    };
+
+    const token = JWT.sign(payload, process.env.ACCESS_TOKEN_KEY);
+    response.cookie("jwt", token, { httpOnly: true, secure: true });
+
+    response.sendStatus(201);
   } catch (error) {
     console.log(error);
+    response.status(500).json({ error: "Failed to create user" });
   }
-  response.sendStatus(201);
 }
 
 module.exports = handleSignup;
