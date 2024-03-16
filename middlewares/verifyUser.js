552cb6643c66734408189d7999e34286b2796df1
Mahesh Sangle <maheshsangle431@gmail.com>
2024-03-16 20:43:25 +0530

    verify user by jwt

---
+++ middlewares/verifyUser.js
@@ -0,0 +1,23 @@
+const JWT = require("jsonwebtoken");
+
+function verifyToken(request, response, next) {
+  const token =
+    request.cookies.access_token ||
+    request.headers["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return response.status(401).json({ error: "Token not provided" });
+  }
+
+  JWT.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      return response
+        .status(403)
+        .json({ error: "Failed to authenticate token" });
+    }
+    request.user = decoded;
+    next();
+  });
+}
+
+module.exports = verifyToken;
