6e6837d9631c0615207410c021ec4dfdaef5bbdf
Mahesh Sangle <maheshsangle431@gmail.com>
2024-03-24 11:59:04 +0530

    updated

--- routes/router.js
+++ routes/router.js
@@ -2,14 +2,13 @@ const express = require("express");
 const handleSignUp = require("../controllers/signUpHandler");
 const handleSignIn = require("../controllers/signInHandler");
 const signUpValidate = require("../middlewares/signUpValidation");
-const verifyToken = require("../middlewares/verifyUser");
-const getUser = require("../controllers/userProfile");
+const verifyToken = require("../controllers/verifyUser");
 const signInValidate = require("../middlewares/signInValidation");
 
 const router = express.Router();
 
 router.post("/signup", signUpValidate, handleSignUp);
 router.post("/signin", signInValidate, handleSignIn);
-router.get("/profile", verifyToken, getUser);
+router.get("/profile", verifyToken);
 
 module.exports = router;
